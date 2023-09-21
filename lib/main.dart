15c04e843dee08f4b71e715184761225fe9e6662
FadyShehataa <fadyshehata40@gmail.com>
2023-09-21 11:31:32 +0300

    setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rick_and_morty_app/Core/utils/api_service.dart';
+import 'package:rick_and_morty_app/Core/utils/service_locator.dart';
 import 'package:rick_and_morty_app/Features/Home/data/repos/home_repo_impl.dart';
 
 import 'Core/utils/app_router.dart';
 import 'Features/Home/presentation/manager/character_cubit/character_cubit.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(
     DevicePreview(
       builder: (context) => const RickAndMortyApp(),
@@ -24,7 +24,9 @@ class RickAndMortyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<CharactersCubit>(
-          create: (context) => CharactersCubit(HomeRepoImpl(ApiService(Dio())))..fetchFeaturedBooks(),
+          create: (context) => CharactersCubit(
+            getIt.get<HomeRepoImpl>(),
+          )..fetchFeaturedBooks(),
         ),
       ],
       child: MaterialApp.router(
